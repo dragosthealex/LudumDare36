6d3c0b52750074ffa2a6c87498efc4fd18c5712b
sanik <alex.radu.1000@gmail.com>
2016-08-28 13:01:29 +0100

    removed old stuff

---
+++ app/Assets/_Scripts/Weapon_ammo/LaserGun.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform tip;
+
+	public void  Fire() {
+		StopCoroutine ("FireLaser");
+		StartCoroutine ("FireLaser");
+	}
+
+	IEnumerator FireLaser() {
+
+		while (Input.GetKey (KeyCode.Mouse0)) {
+			GameObject bullet = Instantiate (bulletPrefab, tip.position, tip.rotation) as GameObject;
+			/*
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 50)) {
+				// Hit stuff
+				if (hit.rigidbody) {
+					hit.rigidbody.AddExplosionForce (2000f, hit.point, 15);
+				}
+			}*/
+			yield return new WaitForSeconds(0.25f);
+		}
+	}
+}
---
+++ app/Assets/_Scripts/Weapon_ammo/LaserWeapon.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserWeapon : RangedWeapon {
+
+	private LineRenderer line;
+
+	// Use this for initialization
+	void Start () {
+		line = GetComponent<LineRenderer> ();
+		line.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	public void  Fire() {
+		StopCoroutine ("FireLaser");
+		StartCoroutine ("FireLaser");
+	}
+
+	IEnumerator FireLaser() {
+		line.enabled = true;
+
+		while (Input.GetKey (KeyCode.Mouse0)) {
+			line.material.mainTextureOffset = new Vector2 (Time.time, Time.time);
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+
+			line.SetPosition (0, ray.origin);
+
+			if(Physics.Raycast(ray, out hit, 50)) {
+				line.SetPosition(1, hit.point);
+				// Hit stuff
+				if (hit.rigidbody) {
+					hit.rigidbody.AddExplosionForce (2000f, hit.point, 15);
+				}
+			} else {
+				line.SetPosition (1, ray.GetPoint (50));
+			}
+			yield return null;
+		}
+
+		line.enabled = false;
+	}
+}
