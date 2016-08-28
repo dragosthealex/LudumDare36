abd8ed16fe4b2f03400face733bf0b5b55b8cc3e
sanik <alex.radu.1000@gmail.com>
2016-08-28 22:17:39 +0100

    added nice single player

---
+++ app/Assets/_Scripts/animController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animController : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
